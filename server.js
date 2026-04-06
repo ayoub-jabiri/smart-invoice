cd5839bf698533ab15cf57a85d47700306333a1e
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-06 14:59:57 +0100

    Set up the environment

---
+++ server.js
@@ -0,0 +1,21 @@
+// External Modules
+import "dotenv/config";
+import express from "express";
+
+// Internal Modules
+import { dbConnect } from "./config/db.js";
+import userRoutes from "./routes/user.router.js";
+
+const app = express();
+
+// Database Connection
+const PORT = process.env.PORT;
+dbConnect();
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`The server is listening on port ${PORT}`);
+});
